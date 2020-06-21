998cfed7efdf03ab6dbfdf375a625796f35ea324
essennejaye <sjeffers02@gmail.com>
2020-06-21 00:31:24 -0700

    timer quiz end state defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-var timer = 10;
 var score = 0;
 var currentQuestionIndex = 0;
 var startBtn = document.querySelector("#btn");
@@ -7,7 +6,7 @@ var countDownEl = document.querySelector("#timer");
 var quizContentEl = document.getElementById("quiz-content");
 var quizQuestionEl = document.getElementById("quiz-question");
 var quizAnswersEl = document.getElementById("quiz-answers")
-var resultsContainer = document.getElementById("results");
+var quizEndEl = document.getElementById("quiz-end");
 var initialsEl = document.getElementById("intials");
 var submitButton = document.getElementById("submit");
 var questions = [
@@ -19,30 +18,31 @@ var questions = [
             "Hyperlinks and Text Markup Language"
         ],
         answer: 0
+    },
+    // {
+    //     question: "How can you open a link in a new tab/browser window?",
+    //     choices: [
+    //         "<a href'url' target='new'>",
+    //         "<a href='url' target='_blank'>",
+    //         "<a href='url' new>",
+    //         "<a href='url' target='new window'>"
+    //     ],
+    //     answer: 1
+    // },
+    // {
+    //     question: "What does CSS stand for?",
+    //     choices: [
+    //         "Creative Sheet Styling",
+    //         "Complicated Style Stuff",
+    //         "Cascading Style Sheets",
+    //         "Contemporary Styling Specifications"
+    //     ],
+    //     answer: 2
 
-    },
-    {
-        question: "How can you open a link in a new tab/browser window?",
-        choices: [
-            "<a href'url' target='new'>",
-            "<a href='url' target='_blank'>",
-            "<a href='url' new>",
-            "<a href='url' target='new window'>"
-        ],
-        answer: 1
-    },
-    {
-        question: "What does CSS stand for?",
-        choices: [
-            "Creative Sheet Styling",
-            "Complicated Style Stuff",
-            "Cascading Style Sheets",
-            "Contemporary Styling Specifications"
-        ],
-        answer: 2
-
-    },
+    // },
 ];
+var timer = questions.length * 15;
+
 function countDown() {
     var countDownTimer = setInterval(function () {
         countDownEl.textContent = timer;
@@ -59,7 +59,7 @@ function buildQuiz() {
     var quizQuestion = questions[currentQuestionIndex].question;
     quizQuestionEl.textContent = quizQuestion;
     var quizChoices = questions[currentQuestionIndex].choices;
-    for (var i = 0; i< quizChoices.length; i++) {
+    for (var i = 0; i < quizChoices.length; i++) {
         var quizChoice = quizChoices[i];
         var listItemEl = document.createElement("li");
         listItemEl.className = "list-choice";
@@ -73,27 +73,48 @@ function buildQuiz() {
     }
 
 }
-function choiceClicked(event){
+function choiceClicked(event) {
     var buttonEl = event.target;
-    if ( buttonEl) {
+    if (buttonEl) {
         var buttonChosen = parseInt(buttonEl.getAttribute("selectedIndex"));
         var answerChoice = questions[currentQuestionIndex].answer;
         if (buttonChosen === answerChoice) {
             score += 5;
-            ++currentQuestionIndex;
-            clearAnswers();
-            buildQuiz();
         }
-        else {
-
+        else if (buttonChosen != answerChoice) {
+            timer -= 10;
+            countDownEl.textContent = timer;
         }
+    }
+    getNextQuestion();
+}
+function getNextQuestion() {
+    if (timer <= 0) {
+        // timer = 0;
+        endQuiz();
+    } else {
+        ++currentQuestionIndex;
+    }
+    if (currentQuestionIndex >= questions.length) {
+        clearAnswers();
+        endQuiz();
+    }
+    else {
+        clearAnswers();
+        buildQuiz();
     }
 }
-function clearAnswers(){
+function clearAnswers() {
     var count = quizAnswersEl.childElementCount;
-    for (var i=0; i<count; i++){
+    for (var i = 0; i < count; i++) {
         quizAnswersEl.removeChild(quizAnswersEl.childNodes[0]);
     }
+}
+function endQuiz() {
+    quizQuestionEl.classList.add("hidden");
+    quizEndEl.classList.remove("hidden");
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = score;
 }
 // function showResults(){};
 
