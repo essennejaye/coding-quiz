f0d7eb7e5162dc7aac2463734e02d76096d583a1
essennejaye <sjeffers02@gmail.com>
2020-06-21 23:04:40 -0700

    added functionality to quiz end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,52 @@
 var score = 0;
+var savedScores = [];
 var currentQuestionIndex = 0;
-var startBtn = document.querySelector("#btn");
+const scoreName = "endscore";
+var startBtn = document.querySelector("#start-button");
+var goBackBtn = document.querySelector("#go-back");
+var clearResultBtn = document.querySelector("#clear-result");
 var startPageEl = document.getElementById("start-page");
 var countDownEl = document.querySelector("#timer");
 var quizContentEl = document.getElementById("quiz-content");
 var quizQuestionEl = document.getElementById("quiz-question");
 var quizAnswersEl = document.getElementById("quiz-answers")
 var quizEndEl = document.getElementById("quiz-end");
-var initialsEl = document.getElementById("intials");
-var submitButton = document.getElementById("submit");
+var submitButton = document.getElementById("submit-button");
 var questions = [
     {
         question: "What does HTML stand for?",
         choices: [
-            "Hyper Text Markup Language",
-            "Home Tool Markup Language",
-            "Hyperlinks and Text Markup Language"
+            "1. Hyper Text Markup Language",
+            "2. Home Tool Markup Language",
+            "3. Hyperlinks and Text Markup Language"
         ],
         answer: 0
     },
-    // {
-    //     question: "How can you open a link in a new tab/browser window?",
-    //     choices: [
-    //         "<a href'url' target='new'>",
-    //         "<a href='url' target='_blank'>",
-    //         "<a href='url' new>",
-    //         "<a href='url' target='new window'>"
-    //     ],
-    //     answer: 1
-    // },
-    // {
-    //     question: "What does CSS stand for?",
-    //     choices: [
-    //         "Creative Sheet Styling",
-    //         "Complicated Style Stuff",
-    //         "Cascading Style Sheets",
-    //         "Contemporary Styling Specifications"
-    //     ],
-    //     answer: 2
+    {
+        question: "How can you open a link in a new tab/browser window?",
+        choices: [
+            "1. <a href'url' target='new'>",
+            "2. <a href='url' target='_blank'>",
+            "3. <a href='url' new>",
+            "4. <a href='url' target='new window'>"
+        ],
+        answer: 1
+    },
+    {
+        question: "What does CSS stand for?",
+        choices: [
+            "1. Creative Sheet Styling",
+            "2. Complicated Style Stuff",
+            "3. Cascading Style Sheets",
+            "4. Contemporary Styling Specifications"
+        ],
+        answer: 2
 
-    // },
+    },
 ];
-var timer = questions.length * 15;
 
 function countDown() {
+    timer = questions.length * 15;
     var countDownTimer = setInterval(function () {
         countDownEl.textContent = timer;
         timer--;
@@ -90,7 +93,7 @@ function choiceClicked(event) {
 }
 function getNextQuestion() {
     if (timer <= 0) {
-        // timer = 0;
+        timer = 0;
         endQuiz();
     } else {
         ++currentQuestionIndex;
@@ -115,11 +118,43 @@ function endQuiz() {
     quizEndEl.classList.remove("hidden");
     var finalScoreEl = document.getElementById("final-score");
     finalScoreEl.textContent = score;
+    // var submitButton = document.querySelector("#submit-button");
+    submitButton.addEventListener("click", getInitials);
 }
-// function showResults(){};
+function getInitials() {
+    var initialsEl = document.getElementById("initials");
+    if (!initialsEl || initialsEl.value === "") {
+        alert("You must enter your initials");
+    } else {
+        var lastHighScore = localStorage.getItem(scoreName);
+        lastHighScoreArray = JSON.parse(lastHighScore);
+        if (!lastHighScoreArray || score > lastHighScoreArray[0].newScore) {
+            var scoreData = {
+                name: initialsEl.value,
+                newScore: score
+            };
+            if (!lastHighScoreArray) lastHighScoreArray = [];
+            lastHighScoreArray.push(scoreData);
+            lastHighScoreArray.sort(function (a, b) { return -(a.newScore - b.newScore)});
+            localStorage.setItem(scoreName, JSON.stringify(lastHighScoreArray));
+        }
+    } showResults();
+}
+function showResults() {
+    quizEndEl.classList.add("hidden");
+    highResultEl = document.getElementById("show-result");
+    highResultEl.classList.remove("hidden");
+    var showHighResultEl = document.getElementById("show-high-result");
+    var lastHighScore = localStorage.getItem(scoreName);
+    lastHighScoreArray = JSON.parse(lastHighScore);
+    if (lastHighScoreArray) {
+        showHighResultEl.textContent = lastHighScoreArray[0].name;
+    }
+};
 
-
-
-
-
+function clearLocalStorage() {
+    localStorage.clear(lastHighScoreArray);
+}
 startBtn.addEventListener("click", countDown);
+goBackBtn.addEventListener("click", countDown);
+clearResultBtn.addEventListener("click", clearLocalStorage);
